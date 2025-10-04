0a9334f2dff9da1efe444ad6289156fd925e7da4
Arno Cornette <ar.cornette@gmail.com>
2025-10-04 15:35:58 +0200

    tech: git based data system

--- backtab/data/backtab_repo.py
+++ backtab/data/backtab_repo.py
@@ -1,2 +1,41 @@
+from backtab.config import APP_CONFIG
+from pathlib import Path
+from os import listdir
+from backtab.members.member import BacktabMember
+from backtab.members.members_loader import BacktabMembersLoader
+from backtab.products.product import BacktabProduct
+from backtab.products.products_loader import BacktabProductsLoader
+
+
 class BacktabDataRepo:
-    pass
+    products: dict[str, BacktabProduct]
+    members: dict[str, BacktabMember]
+
+    def __init__(self):
+        self.products = self.parse_products()
+        self.members = self.parse_members()
+
+    # account: dict[str, Account]
+    def parse_products(self):
+        product_path = f"{APP_CONFIG.data}/products"
+        products_list = listdir(product_path)
+        result: dict[str, BacktabProduct] = {}
+        for path in products_list:
+            with open(f"{product_path}/{path}") as detail:
+                product = BacktabProductsLoader.load_from_json(detail.read())
+                print(product.ean)
+                result["" + product.ean] = product
+        return result
+
+    def parse_members(self):
+        member_path = f"{APP_CONFIG.data}/members"
+        members_list = listdir(member_path)
+        result: dict[str, BacktabMember] = []  # pyright: ignore[reportAssignmentType]
+        for path in members_list:
+            with open(f"{member_path}/{path}") as detail:
+                member = BacktabMembersLoader.load_from_json(detail.read())
+                result[member.account_name] = member
+        return result
+
+
+backtab_repo = BacktabDataRepo()
--- backtab/utils/git.py
+++ backtab/utils/git.py
@@ -1,46 +1,66 @@
-from git import GitCommandError, Repo
-from os import getenv
 import contextlib
+from git import GitCommandError, Repo
+from backtab.config import APP_CONFIG
 from backtab.logger import log
-from backtab.utils.repo import setup_initial_data
+from pathlib import Path
 
-DATA_PATH = getenv("DATA_PATH", "./test")
-git_url = "git@github.com:arnocornette/tab-data"
+initial_folders = ["products", "members"]
+initial_structure: dict[str, list[str]] = {
+    "products": [],
+    "members": [],
+}
 
 
-def setup_repo_folder(path: str):
+def setup_initial_data(path: str):
+    log.info("Setup initial data structure")
+    for folder_key in initial_structure:
+        Path(f"{path}/{folder_key}").mkdir(parents=True, exist_ok=True)
+        for file in initial_structure[folder_key]:
+            with open(f"{path}/{folder_key}/{file}", "w+"):
+                pass
+
+
+def get_or_create_repo(path: str):
     try:
-        return Repo.clone_from(url=git_url, to_path=path)
-    except GitCommandError as git_error:
-        repo = Repo.init(DATA_PATH)
-        remotes = repo.remotes
-        if "test" not in remotes:
-            log.info(f"Creating new remote test with url {git_url}")
-            _ = repo.create_remote("test", git_url)
-
+        print(path)
+        if Path(path).exists():
+            log.info(f"Using existing Repo: {path}")
+            return Repo(path)
+        Path(path).mkdir(exist_ok=True)
+        log.info(f"Cloning from {APP_CONFIG.clone_url}")
+        repo = Repo.clone_from(url=APP_CONFIG.clone_url, to_path=path)
         setup_initial_data(path)
+        return repo
+    except GitCommandError as e:
+        print(e)
+        repo = Repo.init(APP_CONFIG.data)
+        remotes = repo.remotes
+        if "origin" not in remotes:
+            log.info(f"Creating new remote test with url {APP_CONFIG.remote_url}")
+            _ = repo.create_remote("test", APP_CONFIG.remote_url)
+        setup_initial_data(path)
+        log.info("Created and setup data directory")
         return repo
 
 
-repo = setup_repo_folder("./test")
+repo = get_or_create_repo(APP_CONFIG.data)
 
 
-def get_or_pull_repo():
-    return repo.remotes.origin.pull()
+def pull_data():
+    log.info("Pulling changes")
+    _ = repo.remotes.origin.pull()
 
 
-def push_repo(force: bool = False):
-    return repo.remotes.test.push()
+def push():
+    log.info("Pushing changes")
+    _ = repo.remotes.origin.push()
 
 
-# TODO: Switch transacation to a Transaction object
 @contextlib.contextmanager
-def git_transaction(self, transaction: str):
-    head = repo.head
+def commit(transaction: str):
     try:
         yield
-        repo.commit(transaction)
-    except Exception:
-        value = repo.head.reset(index=True, working_tree=True)
-
-    self.synchronized = False
+        _ = repo.commit(transaction)
+    except GitCommandError:
+        log.error("Could not commit transaction")
+        _ = repo.head.reset(index=True, working_tree=True)
