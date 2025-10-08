78b865901581bf680254e16f5c174e06e156693c
Arno Cornette <ar.cornette@gmail.com>
2025-10-08 12:25:43 +0200

    tech: remove inventory from backtab member class

--- backtab/members/member.py
+++ backtab/members/member.py
@@ -1,13 +1,12 @@
 from dataclasses import dataclass
-from beancount.core.inventory import Inventory
+from dataclass_wizard import JSONWizard
 
 
 @dataclass
-class BacktabMember:
+class BacktabMember(JSONWizard):
     name: str
     id: int
     display: str
     pin: str
-    inventory: Inventory
     # assuming only members have member definition
     is_member: bool = False
