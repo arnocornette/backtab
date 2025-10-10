3f15b3dcf2df11ac18ef8c9d3e367482656b7606
Arno Cornette <ar.cornette@gmail.com>
2025-10-10 10:01:53 +0200

    tech: create members and initial steps towards entries from beancount file

--- backtab/data/backtab_repo.py
+++ backtab/data/backtab_repo.py
@@ -1,4 +1,4 @@
-from typing import final
+from typing import Any, final
 
 from backtab.config import APP_CONFIG
 from os import listdir
@@ -15,14 +15,16 @@ from backtab.utils.transaction import add_product
 class BacktabDataRepo:
     products: list[BacktabProduct]
     members: list[BacktabMember]
+    entries: Any
     product_path = f"{APP_CONFIG.data}/products"
     member_path = f"{APP_CONFIG.data}/members"
 
     def __init__(self):
-        self.products = self.parse_products()
-        self.members = self.parse_members()
+        self.products = self.parse_products_from_repo()
+        self.members = self.parse_members_from_repo()
+        self.entries = []
 
-    def parse_products(self):
+    def parse_products_from_repo(self) -> list[BacktabProduct]:
         products_list = listdir(self.product_path)
         result: list[BacktabProduct] = []
         for path in products_list:
@@ -32,19 +34,27 @@ class BacktabDataRepo:
         log.info(f"Loaded {len(result)} products")
         return result
 
-    def parse_members(self):
+    # TODO: should we look into optimizing the reading of files?
+    def parse_members_from_repo(self) -> list[BacktabMember]:
         members_list = listdir(self.member_path)
         result: list[BacktabMember] = []
         for path in members_list:
             with open(f"{self.member_path}/{path}") as detail:
                 member = BacktabMembersLoader.load_from_json(detail.read())
                 result.append(member)
+
+        log.info(f"Loaded {len(result)} members")
         return result
 
     def add_product(self, product: BacktabProduct):
         product_name = product.name.replace(" ", "_").lower()
         with open(f"{self.product_path}/{product_name}.json", "w+") as product_file:
             _ = product_file.write(str(product))
+            _ = commit(add_product)
+
+    def add_member(self, member: BacktabMember):
+        with open(f"{self.member_path}/{member.name}.json", "w+") as member_file:
+            _ = member_file.write(str(member))
             _ = commit(add_product)
 
 
